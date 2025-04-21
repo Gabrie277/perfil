5740cc2d1ddb1d1da55b0db2bddc226bb6906b09
gabrie277 <gabrieloliveiraramos38@gmail.com>
2025-04-21 12:34:57 -0300

    site atualizado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Landing Page</title>
+        <title>Programador de Sistema pra Web</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
+
 export default function Home() {
   return (
     <main>
@@ -10,6 +11,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Contacts />
+     
       
     </main>
   );
